04f12a816285e3d03a1e7b6f4ba0aa35ffd6b1f2
ibareiro98 <ibareiro66@gmail.com>
2024-05-11 10:30:32 -0400

    Creacion de funcionalidad de guardado de producto vista create y logger para la clase productocontroller

--- src/main/java/com/projects/ecommerce/EcommerceApplication.java
+++ src/main/java/com/projects/ecommerce/EcommerceApplication.java
@@ -1,7 +1,11 @@
 package com.projects.ecommerce;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -9,5 +13,14 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
+        objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        return objectMapper;
+    }
 
 }
--- src/main/java/com/projects/ecommerce/controller/ProductController.java
+++ src/main/java/com/projects/ecommerce/controller/ProductController.java
@@ -1,16 +1,45 @@
 package com.projects.ecommerce.controller;
 
+import com.projects.ecommerce.model.Product;
+import com.projects.ecommerce.model.User;
+import com.projects.ecommerce.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
 public class ProductController {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("")
     public String show() {
         return "products/show";
+    }
+
+    @GetMapping("/create")
+    public String create() {
+        return "products/create";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product) {
+
+        LOGGER.info("Este es el objeto product {}", product);
+        User user = new User(1, "", "" ,"" ,"", "", "", "");
+        product.setUser(user);
+
+        productService.save(product);
+
+        return "redirect:/products";
     }
 
 }
